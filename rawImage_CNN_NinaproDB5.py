eb2edf2fd3ca6f9c984ad284e8af62ee1ce17bc0
Jehan Yang <jehanyang@gmail.com>
2023-12-30 01:04:05 -0500

    speedup: Add pin memory to data loaders which should create speedup

--- rawImage_CNN_NinaproDB5.py
+++ rawImage_CNN_NinaproDB5.py
@@ -213,10 +213,10 @@ for name, param in model.named_parameters():
         param.requires_grad = False
 
 batch_size = 64
-train_loader = DataLoader(list(zip(X_train, Y_train)), batch_size=batch_size, shuffle=True, num_workers=4, worker_init_fn=ut_NDB5.seed_worker)
-val_loader = DataLoader(list(zip(X_validation, Y_validation)), batch_size=batch_size, num_workers=4, worker_init_fn=ut_NDB5.seed_worker)
+train_loader = DataLoader(list(zip(X_train, Y_train)), batch_size=batch_size, shuffle=True, num_workers=4, worker_init_fn=ut_NDB5.seed_worker, pin_memory=True)
+val_loader = DataLoader(list(zip(X_validation, Y_validation)), batch_size=batch_size, num_workers=4, worker_init_fn=ut_NDB5.seed_worker, pin_memory=True)
 if (leaveOut == 0):
-    test_loader = DataLoader(list(zip(X_test, Y_test)), batch_size=batch_size, num_workers=4, worker_init_fn=ut_NDB5.seed_worker)
+    test_loader = DataLoader(list(zip(X_test, Y_test)), batch_size=batch_size, num_workers=4, worker_init_fn=ut_NDB5.seed_worker, pin_memory=True)
 
 # Define the loss function and optimizer
 criterion = nn.CrossEntropyLoss()
