887453b79c0c99f0fe0169618539c3847849272f
Jehan Yang <jehanyang@gmail.com>
2024-02-07 21:30:29 -0500

    feat: add batchnorm and dropout arguments

--- rawImage_CNN_JehanData.py
+++ rawImage_CNN_JehanData.py
@@ -103,6 +103,8 @@ print(f"The value of --number_sequential_layers_to_freeze is {args.number_sequen
 print(f"The value of --freeze_all_layers is {args.freeze_all_layers}")
 print(f"The value of --number_hidden_classifier_layers is {args.number_hidden_classifier_layers}")
 print(f"The value of --hidden_classifier_layer_size is {args.hidden_classifier_layer_size}")
+print(f"The value of --classifier_head_dropout is {args.classifier_head_dropout}")
+print(f"The value of --classifier_head_batchnorm is {args.classifier_head_batchnorm}")
 print(f"The value of --learning_rate is {args.learning_rate}")
 print(f"The value of --random_initialization is {args.random_initialization}")
 print(f"The value of --project_name_suffix is {args.project_name_suffix}")
@@ -754,9 +756,9 @@ if args.freeze_model:
         wandb_runname += '_all'
 if args.number_hidden_classifier_layers > 0:
     wandb_runname += '_hidden-' + str(args.number_hidden_classifier_layers) + '-' + str(args.hidden_classifier_layer_size)
-    if args.hidden_classifier_dropout > 0:
-        wandb_runname += '_dropout-' + str(args.hidden_classifier_dropout)
-    if args.hidden_classifier_batchnorm:
+    if args.classifier_head_dropout > 0:
+        wandb_runname += '_dropout-' + str(args.classifier_head_dropout)
+    if args.classifier_head_batchnorm:
         wandb_runname += '_batchnorm'
 wandb_runname += '_lr-' + str(args.learning_rate)
 if args.turn_on_rms:
@@ -947,10 +949,10 @@ if args.number_hidden_classifier_layers >= 0:
     layers = []
     for hidden_size in range(args.number_hidden_classifier_layers):
         layers.append(nn.Linear(in_features, args.hidden_classifier_layer_size))
-        if args.batch_norm:
+        if args.classifier_head_batchnorm:
             layers.append(nn.BatchNorm1d(args.hidden_classifier_layer_size))
         layers.append(nn.ReLU())
-        if args.dropout > 0:
+        if args.classifier_head_dropout > 0:
             layers.append(nn.Dropout(args.dropout))
         in_features = args.hidden_classifier_layer_size
         
@@ -967,9 +969,7 @@ if args.number_hidden_classifier_layers >= 0:
 # Training loop
 gc.collect()
 torch.cuda.empty_cache()
-    
-    # TODO: After fixes, change name from SimCLR-test to turn-on-simclr and wandbrunname to simclr-epochs-X
-    
+        
 if leaveOut != 0:
     run = wandb.init(name=wandb_runname, project='emg_benchmarking_LOSO_JehanDataset' + args.project_name_suffix, entity='jehanyang')
 else:
