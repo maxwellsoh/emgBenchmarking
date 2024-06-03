fixing target normalization

CNN_EMG.py
@@ -24,15 +24,15 @@ import gc
 import datetime
 from PIL import Image
 from torch.utils.data import Dataset
-import VisualTransformer
+#import VisualTransformer
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from joblib import dump
 from sklearn.metrics import accuracy_score, log_loss
 import torch.nn.functional as F
-from semilearn import get_dataset, get_data_loader, get_net_builder, get_algorithm, get_config, Trainer, split_ssl_data, BasicDataset
-from semilearn.core.utils import send_model_cuda
+#from semilearn import get_dataset, get_data_loader, get_net_builder, get_algorithm, get_config, Trainer, split_ssl_data, BasicDataset
+#from semilearn.core.utils import send_model_cuda
 import torchmetrics
 import ml_metrics_utils as ml_utils
 from sklearn.metrics import confusion_matrix, classification_report
@@ -340,7 +340,7 @@ if exercises:
     with multiprocessing.Pool(processes=multiprocessing.cpu_count()//8) as pool:
         for exercise in args.exercises:
             if (args.target_normalize > 0):
-                mins, maxes = utils.getExtrema(args.leftout_subject, args.target_normalize, exercise)
+                mins, maxes = utils.getExtrema(args.leftout_subject+1, args.target_normalize, exercise)
                 emg_async = pool.map_async(utils.getEMG, [(i+1, exercise, mins, maxes, args.leftout_subject + 1) for i in range(utils.num_subjects)])
             else:
                 emg_async = pool.map_async(utils.getEMG, list(zip([(i+1) for i in range(utils.num_subjects)], exercise*np.ones(utils.num_subjects).astype(int))))
@@ -413,15 +413,24 @@ else: # Not exercises
     if (args.target_normalize > 0):
         with multiprocessing.Pool(processes=multiprocessing.cpu_count()//8) as pool:
             if args.leave_one_session_out:
-                NotImplementedError("leave-one-session-out not implemented with target_normalize yet")
-
-            mins, maxes = utils.getExtrema(args.leftout_subject, args.target_normalize)
-            
-            emg_async = pool.map_async(utils.getEMG, [(i+1, mins, maxes, args.leftout_subject + 1) for i in range(utils.num_subjects)])
-            emg = emg_async.get() # (SUBJECT, TRIAL, CHANNEL, TIME)
-            
-            labels_async = pool.map_async(utils.getLabels, [(i+1) for i in range(utils.num_subjects)])
-            labels = labels_async.get()
+                total_number_of_sessions = 2
+                mins, maxes = utils.getExtrema(args.leftout_subject+1, args.target_normalize, lastSessionOnly=True)
+                emg = []
+                labels = []
+                for i in range(1, total_number_of_sessions+1):
+                    emg_async = pool.map_async(utils.getEMG_separateSessions, [(j+1, i, mins, maxes, args.leftout_subject + 1) for j in range(utils.num_subjects)])
+                    emg.extend(emg_async.get())
+                    
+                    labels_async = pool.map_async(utils.getLabels_separateSessions, [(j+1, i) for j in range(utils.num_subjects)])
+                    labels.extend(labels_async.get())
+            else:
+                mins, maxes = utils.getExtrema(args.leftout_subject+1, args.target_normalize)
+                
+                emg_async = pool.map_async(utils.getEMG, [(i+1, mins, maxes, args.leftout_subject + 1) for i in range(utils.num_subjects)])
+                emg = emg_async.get() # (SUBJECT, TRIAL, CHANNEL, TIME)
+                
+                labels_async = pool.map_async(utils.getLabels, [(i+1) for i in range(utils.num_subjects)])
+                labels = labels_async.get()
     else: # Not target_normalize
         with multiprocessing.Pool(processes=multiprocessing.cpu_count()//8) as pool:
             if args.leave_one_session_out: # based on 2 sessions for each subject

utils_CapgMyo.py
@@ -122,8 +122,8 @@ def getData (subject, gesture, trial, session=None):
         sub = str(subject[0])
         if subject[0] < 10:
             sub = '0' + sub
-        target_max = subject[1]
-        target_min = subject[2]
+        target_min = subject[1]
+        target_max = subject[2]
         leftout = subject[3]
 
     name = '0' + sub + '-00' + str(gesture) + '-00' +str(trial)
@@ -153,7 +153,7 @@ def getEMG_separateSessions(args):
     data_index = participants_first_session_index[subject_number-1] if session_number == 1 else participants_second_session_index[subject_number-1]
     return filter(getData(data_index, 1, 1))
 
-def getExtrema (n, p):
+def getExtrema (n, p, lastSessionOnly=False):
     mins = np.zeros((numElectrodes, numGestures))
     maxes = np.zeros((numElectrodes, numGestures))
     

utils_Hyser.py
@@ -99,7 +99,7 @@ def target_normalize (data, target_min, target_max, gesture):
         - source_min[i])) * (target_max[i][gesture] - target_min[i][gesture]) + target_min[i][gesture]
     return data
 
-def getEMG_help (sub, session, target_max=None, target_min=None, leftout=None, unfold=True):
+def getEMG_help (sub, session, target_min=None, target_max=None, leftout=None, unfold=True):
     emg = []
 
     currFile = 1
@@ -151,13 +151,13 @@ def getEMG_help (sub, session, target_max=None, target_min=None, leftout=None, u
 def getEMG (args):
     if (type(args) == int):
         n = args
-        target_max = None
         target_min = None
+        target_max = None
         leftout = None
     else:
         n = args[0]
-        target_max = args[1]
-        target_min = args[2]
+        target_min = args[1]
+        target_max = args[2]
         leftout = args[3]
     
     if (n < 10):
@@ -165,22 +165,21 @@ def getEMG (args):
     else:
         sub = f'{n}'
 
-    emg = getEMG_help(sub, "1", target_max, target_min, leftout) + getEMG_help(sub, "2", target_max, target_min, leftout)
+    emg = getEMG_help(sub, "1", target_min, target_max, leftout) + getEMG_help(sub, "2", target_min, target_max, leftout)
     return filter(torch.cat(emg, dim=0))
 
 def getEMG_separateSessions(args):
     if (len(args) == 2):
         subject_number = args[0]
         session_number = args[1]
-        target_max = None
         target_min = None
+        target_max = None
         leftout = None
-        
     else:
         subject_number = args[0]
         session_number = args[1]
-        target_max = args[2]
-        target_min = args[3]
+        target_min = args[2]
+        target_max = args[3]
         leftout = args[4]
     
     if (subject_number < 10):
@@ -188,12 +187,12 @@ def getEMG_separateSessions(args):
     else:
         sub = f'{subject_number}'
 
-    emg = getEMG_help(sub, str(session_number), target_max, target_min, leftout)
+    emg = getEMG_help(sub, str(session_number), target_min, target_max, leftout)
     emg = filter(torch.cat(emg, dim=0))
     return emg
         
     
-def getExtrema (n, p):
+def getExtrema (n, p, lastSessionOnly=False):
     mins = np.zeros((numElectrodes, numGestures))
     maxes = np.zeros((numElectrodes, numGestures))
 
@@ -202,8 +201,12 @@ def getExtrema (n, p):
     else:
         sub = f'{n}'
 
-    emg = getEMG_help(sub, "1", unfold=False) + getEMG_help(sub, "2", unfold=False)
-    labels = getLabels(n, unfold=False)
+    if lastSessionOnly:
+        emg = getEMG_help(sub, "2", unfold=False)
+        labels = getLabels_separateSessions((n, 2), unfold=False)
+    else:
+        emg = getEMG_help(sub, "1", unfold=False)
+        labels = getLabels_separateSessions((n, 1), unfold=False)
 
     for i in range(numGestures):
         subEMG = [emg[j] for j in range(len(emg)) if labels[j, i] == 1.0]
@@ -246,13 +249,9 @@ def getLabels (n, unfold=True):
 
     return contract(labels)
 
-def getLabels_separateSessions(args):
-    if (len(args) == 2):
-        subject_number = args[0]
-        session_number = args[1]
-    else:
-        subject_number = args[0]
-        session_number = args[1]
+def getLabels_separateSessions(args, unfold=True):
+    subject_number = args[0]
+    session_number = args[1]
     
     if (subject_number < 10):
         sub = f'0{subject_number}'
@@ -264,8 +263,12 @@ def getLabels_separateSessions(args):
     vals = file.readline().strip().split(',')
     for v in vals:
         if (v in gesture_nums):
-            for i in range(7):
+            if unfold:
+                for i in range(7):
+                    labels.append(v)
+            else:
                 labels.append(v)
+                
     file.close()
     return contract(labels)
 