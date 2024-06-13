a91bf2a8e03d882e4b1acf3e36e166b3ee8f0461
jehanyang <jehanyang@gmail.com>
2024-06-13 00:24:06 -0400

    feat: download FlexWear-HD dataset automatically

--- get_datasets/get_Jehan_Dataset.sh
+++ get_datasets/get_Jehan_Dataset.sh
@@ -1,1 +1,4 @@
-echo "Jehan Dataset must be requested."
+echo "Retrieving FlexWear-HD_Dataset..."
+git clone --verbose https://huggingface.co/datasets/jehanyang/FlexWear-HD
+
+echo "Done retrieving FlexWear-HD_Dataset."
