953bce100fb360a3990db8421b25815c831be562
Vivianna Lieu <lieu.vivianna@gmail.com>
2024-07-15 02:11:51 -0400

    Data initialize refactoring Added hook for data_initialize. Data classes defined in Data/ folder. X_Data, Y_Data, and Label_Data are responsible for loading their respective data. Eliminates the need for casing on force_regression.

---
+++ Data/Label_Data.py
@@ -0,0 +1,117 @@
+"""
+Label_Data.py
+- Contains Label_Data class, which is a subclass of Data.
+- Label_Data is responsible for loading and processing labels of the given dataset.
+"""
+import torch
+import numpy as np
+from .Data import Data
+import multiprocessing
+
+class Label_Data(Data):
+
+    def __init__(self, env):
+            super().__init__("Label",env)
+
+            # Set seeds for reproducibility
+            np.random.seed(self.args.seed)
+            torch.manual_seed(self.args.seed)
+            torch.cuda.manual_seed(self.args.seed)
+            if torch.cuda.is_available():
+                torch.cuda.manual_seed_all(self.args.seed)
+            torch.backends.cudnn.deterministic = True
+            torch.backends.cudnn.benchmark = False
+
+    def load_data(self, exercises):
+        """Sets self.data to labels. """
+
+        def get_labels_ninapro():
+
+            if self.args.multiprocessing:
+                print("Multiprocessing occasionally has issues. If timing out, try running with multiprocesing=False")
+                labels = []
+                with multiprocessing.Pool(processes=multiprocessing.cpu_count()//8) as pool:
+                    for exercise in self.args.exercises:
+                        labels_async = pool.map_async(self.utils.getLabels, list(zip([(i+1) for i in range(self.utils.num_subjects)], exercise*np.ones(self.utils.num_subjects).astype(int), [self.args]*self.utils.num_subjects)))
+                        labels.append(labels_async.get())
+            else: 
+                labels = []
+                for exercise in self.args.exercises:
+                    exercise_labels = []
+                    for i in range(self.utils.num_subjects):
+                        label = self.utils.getLabels((i+1, exercise, self.args))
+                        exercise_labels.append(label)
+                    labels.append(exercise_labels)
+                
+            return labels
+
+            
+        def get_labels_other_datasets():
+            with multiprocessing.Pool(processes=multiprocessing.cpu_count()//8) as pool:
+                if self.args.leave_one_session_out:
+                    labels = []
+                    for i in range(1, self.utils.num_sessions+1):
+                        labels_async = pool.map_async(self.utils.getLabels_separateSessions, [(j+1, i) for j in range(self.utils.num_subjects)])
+                        labels.extend(labels_async.get())
+                else:
+                    labels_async = pool.map_async(self.utils.getLabels, [(i+1) for i in range(self.utils.num_subjects)])
+                    labels = labels_async.get()
+            
+            return labels
+
+        if exercises:
+            self.data = get_labels_ninapro()
+        else: 
+            self.data = get_labels_other_datasets()
+        
+    # Process Ninapro Helper
+    def append_to_trials(self, exercise_set, subject):
+        """Appends EMG data for a given subject across all exercises to self.X.subject_trials. Helper function for process_ninapro.
+        """
+        self.subject_trials.append(self.data[exercise_set][subject])
+
+    def concat_across_exercises(self):
+
+        """Concatenates label data across exercises for a given subject.
+        Helper function for process_ninapro.
+        """
+
+        # Convert from one hot encoding to labels
+        # Assuming labels are stored separately and need to be concatenated end-to-end
+
+
+        labels_set = []
+        index_to_start_at = 0
+        for i in range(len(self.subject_trials)):
+            subject_labels_to_concatenate = [x + index_to_start_at if x != 0 else 0 for x in np.argmax(self.subject_trials[i], axis=1)]
+            if self.args.dataset == "ninapro-db5":
+                index_to_start_at = max(subject_labels_to_concatenate)
+            labels_set.append(subject_labels_to_concatenate)
+
+        if self.args.partial_dataset_ninapro:
+            desired_gesture_labels = self.utils.partial_gesture_indices
+
+        concatenated_labels = np.concatenate(labels_set, axis=0) # (TRIAL)
+
+        indices_for_partial_dataset = None
+        if self.args.partial_dataset_ninapro:
+            indices_for_partial_dataset = np.array([indices for indices, label in enumerate(concatenated_labels) if label in desired_gesture_labels])
+            
+            concatenated_labels = concatenated_labels[indices_for_partial_dataset]
+        
+            # convert labels to indices
+            label_to_index = {label: index for index, label in enumerate(desired_gesture_labels)}
+            concatenated_labels = [label_to_index[label] for label in concatenated_labels]
+        
+
+        # Convert to one hot encoding
+        concatenated_labels = np.eye(np.max(concatenated_labels) + 1)[concatenated_labels] # (TRIAL, GESTURE)
+
+        self.concatenated_trials = concatenated_labels
+
+        return indices_for_partial_dataset
+        
+
+    def append_flexwear_unlabeled_to_finetune_unlabeled_list(self, flexwear_unlabeled_data):
+        assert self.args.load_unlabeled_data_flexwearhd, "Cannot append unlabeled data if load_unlabeled_data_flexwearhd is turned off."
+        self.finetune_unlabeled_list.append(np.zeros(flexwear_unlabeled_data.shape[0]))
---
+++ Data/X_Data.py
@@ -0,0 +1,233 @@
+"""
+X_Data.py
+- This file contains the class definition for the X_Data class, which is a subclass of the Data class.
+- The X_Data class is used to load and process EMG data for the given dataset.
+"""
+import torch
+import numpy as np
+from .Data import Data
+import multiprocessing
+
+from tqdm import tqdm
+import os
+import zarr
+
+class X_Data(Data):
+
+    def __init__(self, env):
+        super().__init__("X", env)
+
+        # Set seeds for reproducibility
+        np.random.seed(self.args.seed)
+        torch.manual_seed(self.args.seed)
+        torch.cuda.manual_seed(self.args.seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(self.args.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+        # EMG specific values
+        self.width = None
+        self.length = None
+
+        self.global_low_value = None
+        self.global_high_value = None
+        self.scaler = None
+        self.train_indices = None
+        self.validation_indices = None
+
+    # Load EMG Data
+    def load_data(self, exercises):
+        """ Sets self.data to EMG data. (emg) """
+
+        def load_EMG_ninapro():
+            """Gets the EMG data for Ninapro datasets.
+
+            Returns:
+                emg (EXERCISE SET, SUBJECT, TRIAL, CHANNEL, TIME): EMG data for the dataset with target_normalization (if applicable).
+            """
+
+            emg = []
+            with multiprocessing.Pool(processes=multiprocessing.cpu_count()//8) as pool:
+                for exercise in self.args.exercises:
+                    if (self.args.target_normalize > 0):
+                        mins, maxes = self.utils.getExtrema(self.args.leftout_subject, self.args.target_normalize, exercise, self.args)
+                        emg_async = pool.map_async(self.utils.getEMG, [(i+1, exercise, mins, maxes, self.args.leftout_subject, self.args) for i in range(self.utils.num_subjects)])
+
+                    else:
+                        emg_async = pool.map_async(self.utils.getEMG, [(i+1, exercise, self.args) for i in range(self.utils.num_subjects)])
+
+                    emg.append(emg_async.get()) # (EXERCISE SET, SUBJECT, TRIAL, CHANNEL, TIME)
+
+            return emg
+
+        def load_EMG_other_datasets(): 
+            """Loads the EMG data for other, non Ninapro datasets.
+
+            Returns:
+                emg (SUBJECT, TRIAL, CHANNEL, TIME STEP): EMG data for the dataset with target_normalization (if applicable). 
+            """
+            emg = []
+            if (self.args.target_normalize > 0):
+                with multiprocessing.Pool(processes=multiprocessing.cpu_count()//8) as pool:
+                    mins, maxes = self.utils.getExtrema(self.args.leftout_subject, self.args.target_normalize)
+                    if self.args.leave_one_session_out:
+                        emg = []
+                        labels = []
+                        for i in range(1, self.utils.num_sessions+1):
+                            emg_async = pool.map_async(self.utils.getEMG_separateSessions, [(j+1, i, mins, maxes, self.args.leftout_subject) for j in range(self.utils.num_subjects)])
+                            emg.extend(emg_async.get())
+
+                    else:
+                        emg_async = pool.map_async(self.utils.getEMG, [(i+1, mins, maxes, self.args.leftout_subject + 1) for i in range(self.utils.num_subjects)])
+
+                        emg = emg_async.get() # (SUBJECT, TRIAL, CHANNEL, TIME)
+            else: 
+                with multiprocessing.Pool(processes=multiprocessing.cpu_count()//8) as pool:
+                    if self.args.leave_one_session_out:
+            
+                        emg = []
+                        labels = []
+                        for i in range(1, self.utils.num_sessions+1):
+                            emg_async = pool.map_async(self.utils.getEMG_separateSessions, [(j+1, i) for j in range(self.utils.num_subjects)])
+                            emg.extend(emg_async.get())
+                
+                    else: # Not leave one session out
+                        emg_async = pool.map_async(self.utils.getEMG, [(i+1) for i in range(self.utils.num_subjects)])
+                        emg = emg_async.get() # (SUBJECT, TRIAL, CHANNEL, TIME)
+            return emg
+
+        if exercises:
+            # Ninapro datasets have to be processed.
+            self.data = load_EMG_ninapro()
+        else:
+
+            self.data = load_EMG_other_datasets()
+
+
+    def print_data_information(self):
+
+        print("Number of Samples (across all participants): ", sum([e.shape[0] for e in self.data]))
+
+
+        self.length = self.data[0].shape[1]
+        self.width = self.data[0].shape[2]
+        print("Number of Electrode Channels (length of EMG): ", self.length)
+        print("Number of Timesteps per Trial (width of EMG):", self.width)
+
+
+    # Process Ninapro Helper
+    def append_to_trials(self, exercise_set, subject):
+        """Appends EMG data for a given subject across all exercises to self.X.subject_trials. Helper function for process_ninapro.
+        """
+        self.subject_trials.append(self.data[exercise_set][subject])
+
+    # Helper for loading EMG data for Ninapro
+    def concat_across_exercises(self, indices_for_partial_dataset=None):
+        """Concatenates EMG data across exercises for a given subject.
+        Helper function for process_ninapro.
+        """
+        self.concatenated_trials = np.concatenate(self.subject_trials, axis=0)
+
+        if self.args.partial_dataset_ninapro:
+            self.concatenated_trials = self.concatenated_trials[indices_for_partial_dataset]
+
+    
+    def load_images(self, base_foldername_zarr):
+        """Updates self.data to be the loaded images for EMG data. Returns flexwear_unlabeled_data if self.args.load_unlabeled_data_flexwearhd.
+        
+        If dataset exists, loads images. Otherwise, creates imaeges and saves in directory. 
+
+        Returns:
+            flexwear_unlabeled_data: unlabeled data if self.args.load_unlabeled_data_flexwearhd
+        """
+        assert self.utils is not None, "self.utils is not defined. Please run initialize() first."
+
+        flexwear_unlabeled_data = None
+
+        emg = self.data # should already be defined as emg using load_data
+        image_data = []
+        for x in tqdm(range(len(emg)), desc="Number of Subjects "):
+            if self.args.held_out_test:
+                subject_folder = f'subject{x}/'
+            elif self.args.leave_one_session_out:
+                subject_folder = f'session{x}/'
+            else:
+                subject_folder = f'LOSO_subject{x}/'
+            foldername_zarr = base_foldername_zarr + subject_folder
+            
+            subject_or_session = "session" if self.args.leave_one_session_out else "subject"
+            print(f"Attempting to load dataset for {subject_or_session}", x, "from", foldername_zarr)
+
+            print("Looking in folder: ", foldername_zarr)
+            # Check if the folder (dataset) exists, load if yes, else create and save
+            if os.path.exists(foldername_zarr):
+                # Load the dataset
+                dataset = zarr.open(foldername_zarr, mode='r')
+                print(f"Loaded dataset for {subject_or_session} {x} from {foldername_zarr}")
+                if self.args.load_few_images:
+                    image_data += [dataset[:10]]
+                else: 
+                    image_data += [dataset[:]]
+            else:
+                print(f"Could not find dataset for {subject_or_session} {x} at {foldername_zarr}")
+                # Get images and create the dataset
+                if (self.args.target_normalize > 0):
+                    self.scaler = None
+                images = self.utils.getImages(
+                    emg[x], 
+                    self.scaler, 
+                    self.length, 
+                    self.width,
+                    turn_on_rms=self.args.turn_on_rms, 
+                    rms_windows=self.args.rms_input_windowsize,
+                    turn_on_magnitude=self.args.turn_on_magnitude, 
+                    global_min=self.global_low_value, 
+                    global_max=self.global_high_value,
+                    turn_on_spectrogram=self.args.turn_on_spectrogram, 
+                    turn_on_cwt=self.args.turn_on_cwt,
+                    turn_on_hht=self.args.turn_on_hht
+                )
+                images = np.array(images, dtype=np.float16)
+                
+                # Save the dataset
+                if self.args.save_images:
+                    os.makedirs(foldername_zarr, exist_ok=True)
+                    dataset = zarr.open(foldername_zarr, mode='w', shape=images.shape, dtype=images.dtype, chunks=True)
+                    dataset[:] = images
+                    print(f"Saved dataset for subject {x} at {foldername_zarr}")
+                else:
+                    print(f"Did not save dataset for subject {x} at {foldername_zarr} because save_images is set to False")
+                image_data += [images]
+                
+        if self.args.load_unlabeled_data_flexwearhd:
+            unlabeled_images = self.utils.getImages(
+                unlabeled_online_data, 
+                self.scaler, 
+                self.length, 
+                self.width,
+                turn_on_rms=self.args.turn_on_rms, 
+                rms_windows=self.args.rms_input_windowsize,
+                turn_on_magnitude=self.args.turn_on_magnitude, 
+                global_min=self.global_low_value, 
+                global_max=self.global_high_value,
+                turn_on_spectrogram=self.args.turn_on_spectrogram, 
+                turn_on_cwt=self.args.turn_on_cwt,
+                turn_on_hht=self.args.turn_on_hht
+            )
+            unlabeled_images = np.array(unlabeled_images, dtype=np.float16)
+            flexwear_unlabeled_data = unlabeled_images
+            self.flexwear_unlabeled_data = flexwear_unlabeled_data
+            del unlabeled_images, unlabeled_online_data
+
+        self.data = image_data
+        
+
+        return flexwear_unlabeled_data
+        
+    # Helper for leave_one_session_out
+
+    def append_flexwear_unlabeled_to_finetune_unlabeled_list(self, flexwear_unlabeled_data):
+        assert self.args.load_unlabeled_data_flexwearhd, "Cannot append unlabeled data if load_unlabeled_data_flexwearhd is turned off."
+        self.finetune_unlabeled_list.append(flexwear_unlabeled_data)
+   
